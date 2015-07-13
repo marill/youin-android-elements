d3314816179b9d6d4fe504e2c6b522d6c156feec
marill <marilarsen87@gmail.com>
2015-07-13 16:21:30 +0200

    Custom font

--- YouinElements/app/src/main/java/com/youinmedia/youinelements/LoginActivity.java
+++ YouinElements/app/src/main/java/com/youinmedia/youinelements/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.youinmedia.youinelements;
 
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.*;
+import android.content.res.AssetManager;
+import android.widget.EditText;
+
+import java.util.Locale;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -11,5 +16,13 @@ public class LoginActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        EditText un = (EditText)findViewById(R.id.username);
+        EditText pw = (EditText)findViewById(R.id.password);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/Roboto-Regular.ttf");
+
+        un.setTypeface(custom_font);
+        pw.setTypeface(custom_font);
     }
 }
---
+++ YouinElements/app/src/main/java/com/youinmedia/youinelements/YouinApplication.java
@@ -0,0 +1,20 @@
+package com.youinmedia.youinelements;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+
+import java.util.Locale;
+
+public class YouinApplication extends Application {
+
+    public void onCreate(){
+        super.onCreate();
+
+
+
+    }
+
+
+}
