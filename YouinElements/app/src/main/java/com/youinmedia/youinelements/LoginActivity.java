2dfb442c1152ae78c69065df96cea74b93207142
marill <marilarsen87@gmail.com>
2015-07-13 15:00:44 +0200

    intial commit

---
+++ YouinElements/app/src/main/java/com/youinmedia/youinelements/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.youinmedia.youinelements;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.*;
+
+
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
