f4b74b6c43e211d50970669e0cb858e31097d4ab
marill <marilarsen87@gmail.com>
2015-07-14 16:11:04 +0200

    action bar

--- YouinElements/app/src/main/java/com/youinmedia/youinelements/AddFriendsActivity.java
+++ YouinElements/app/src/main/java/com/youinmedia/youinelements/AddFriendsActivity.java
@@ -1,15 +1,22 @@
 package com.youinmedia.youinelements;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class AddFriendsActivity extends AppCompatActivity {
+public class AddFriendsActivity extends AppCompatActivity implements ActionBar.TabListener{
     Button facebookButton;
     Button lightningGroupsButton;
 
@@ -17,6 +24,18 @@ public class AddFriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friends);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // for each of the sections in the app, add a tab to the action bar.
+        actionBar.addTab(actionBar.newTab().setText("ADD FRIENDS")
+                .setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("GROUPS")
+                .setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("FRIENDS")
+                .setTabListener(this));
 
         facebookButton = (Button)findViewById(R.id.facebook_button);
         lightningGroupsButton = (Button)findViewById(R.id.lightning_groups_button);
@@ -27,5 +46,48 @@ public class AddFriendsActivity extends AppCompatActivity {
         lightningGroupsButton.setTypeface(roboto_font);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_friends, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab,
+                              FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, show the tab contents in the
+        // container vie
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab,
+                                FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab,
+                                FragmentTransaction fragmentTransaction) {
     }
 }
